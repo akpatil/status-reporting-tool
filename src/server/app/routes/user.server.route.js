e5766357860641c5fb31e9c714f354b98b203293
akshaypatilofficial <akshaypatilofficial93@gmail.com>
2016-02-25 23:12:50 +0530

    Added User controller

--- src/server/app/routes/user.server.route.js
+++ src/server/app/routes/user.server.route.js
@@ -1,5 +1,7 @@
-var User = require('../models/user.server.model');
+var User = require('../controllers/user.server.controller');
 
 module.exports = function(app){
-	
+	app.route('/user').get(User.list).post(User.create);
+	app.route('/user/:userID').put(User.update).delete(User.delete);
+	app.param('userID', User.readByID);
 };
